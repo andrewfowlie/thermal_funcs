02891e44d5f2a3afef77d1f5745b133276af5743
andrew <andrew.j.fowlie@googlemail.com>
2017-07-08 19:13:12 +1000

    Use bessel in mathematica

--- src/math.c
+++ src/math.c
@@ -4,11 +4,11 @@ double J_F_quad(double y_squared, double abs_error, double rel_error, int max_n)
 double J_B_quad(double y_squared, double abs_error, double rel_error, int max_n);
 
 int J_F(double y_squared) {
-   return J_F_quad(y_squared, 1E-8, 1E-8, 10000);
+   return J_F_bessel(y_squared, 1E-8, 1E-8, 10000);
 }
 
 int J_B(double y_squared) {
-   return J_B_quad(y_squared, 1E-8, 1E-8, 10000);
+   return J_B_bessel(y_squared, 1E-8, 1E-8, 10000);
 }
 
 int main(int argc, char *argv[]) {
