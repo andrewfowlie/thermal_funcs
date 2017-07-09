dacf280cbad2154520a3606a978014abb682fb99
andrew <andrew.j.fowlie@googlemail.com>
2017-07-09 15:38:04 +1000

    Improved Mathematia interface

--- src/math.c
+++ src/math.c
@@ -1,14 +1,13 @@
-#include "wstp.h"
+#include <stdbool.h>
+#include <wstp.h>
+#include <thermal_funcs.h>
 
-double J_F_quad(double y_squared, double abs_error, double rel_error, int max_n);
-double J_B_quad(double y_squared, double abs_error, double rel_error, int max_n);
-
-int J_F(double y_squared) {
-   return J_F_bessel(y_squared, 1E-8, 1E-8, 10000);
+double J_F(double y_squared) {
+   return J_F_bessel(y_squared, 1E-7, 1E-7, 10000, false);
 }
 
-int J_B(double y_squared) {
-   return J_B_bessel(y_squared, 1E-8, 1E-8, 10000);
+double J_B(double y_squared) {
+   return J_B_bessel(y_squared, 1E-7, 1E-7, 10000, false);
 }
 
 int main(int argc, char *argv[]) {
--- src/thermal_funcs.h
+++ src/thermal_funcs.h
@@ -7,41 +7,48 @@
 
 
 #ifdef __cplusplus
+#define DEFAULT(x) = x
+#else
+#define DEFAULT(x)
+#endif
+
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 double J_B_quad(double y_squared,
-                double abs_error = 1E-7,
-                double rel_error = 1E-7,
-                int max_n = 10000);
+                double abs_error DEFAULT(1E-7),
+                double rel_error DEFAULT(1E-7),
+                int max_n DEFAULT(10000));
 double J_F_quad(double y_squared,
-                double abs_error = 1E-7,
-                double rel_error = 1E-7,
-                int max_n = 10000);
+                double abs_error DEFAULT(1E-7),
+                double rel_error DEFAULT(1E-7),
+                int max_n DEFAULT(10000));
 double J_B_taylor(double y_squared,
-                  double abs_error = 1E-7,
-                  double rel_error = 1E-7,
-                  int max_n = 10000);
+                  double abs_error DEFAULT(1E-7),
+                  double rel_error DEFAULT(1E-7),
+                  int max_n DEFAULT(10000));
 double J_F_taylor(double y_squared,
-                  double abs_error = 1E-7,
-                  double rel_error = 1E-7,
-                  int max_n = 10000);
+                  double abs_error DEFAULT(1E-7),
+                  double rel_error DEFAULT(1E-7),
+                  int max_n DEFAULT(10000));
 double J_F_bessel(double y_squared,
-                  double abs_error = 1E-7,
-                  double rel_error = 1E-7,
-                  int max_n = 10000,
-                  bool fast = false);
+                  double abs_error DEFAULT(1E-7),
+                  double rel_error DEFAULT(1E-7),
+                  int max_n DEFAULT(10000),
+                  bool fast DEFAULT(false));
 double J_B_bessel(double y_squared,
-                  double abs_error = 1E-7,
-                  double rel_error = 1E-7,
-                  int max_n = 10000,
-                  bool fast = false);
-double J_B_lim(double y_squared, bool upper = true);
-double J_F_lim(double y_squared, bool upper = true);
+                  double abs_error DEFAULT(1E-7),
+                  double rel_error DEFAULT(1E-7),
+                  int max_n DEFAULT(10000),
+                  bool fast DEFAULT(false));
+double J_B_lim(double y_squared, bool upper DEFAULT(true));
+double J_F_lim(double y_squared, bool upper DEFAULT(true));
 double J_B_approx(double y_squared);
 double J_F_approx(double y_squared);
-double J_B_zeta(double y_squared, int n = 25);
-double J_F_zeta(double y_squared, int n = 25);
+double J_B_zeta(double y_squared, int n DEFAULT(25));
+double J_F_zeta(double y_squared, int n DEFAULT(25));
 
 #ifdef __cplusplus
 }
