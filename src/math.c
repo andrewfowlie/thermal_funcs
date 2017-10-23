1f4d2c7e43d0a8e69a1e8f76dac3dfa59d47d888
andrew <andrew.j.fowlie@googlemail.com>
2017-10-24 09:29:13 +1100

    Mathematica interface to derivatives

--- src/math.c
+++ src/math.c
@@ -1,13 +1,31 @@
 #include <stdbool.h>
 #include <wstp.h>
 #include <thermal_funcs.h>
+#include <derivatives.h>
 
-double J_F(double y_squared) {
-   return J_F_bessel(y_squared, 1E-7, 1E-7, 10000, false);
+
+double J_F(double y_squared, int n) {
+   if (n == 0) {
+      return J_F_bessel(y_squared, 1E-7, 1E-7, 10000, false);
+   } else if (n == 1) {
+      return D1_J_F_bessel(y_squared, 1E-7, 1E-7, 10000);
+   } else if (n == 2) {
+      return D2_J_F_bessel(y_squared, 1E-7, 1E-7, 10000);
+   } else {
+      return -1;
+   }
 }
 
-double J_B(double y_squared) {
-   return J_B_bessel(y_squared, 1E-7, 1E-7, 10000, false);
+double J_B(double y_squared, int n) {
+   if (n == 0) {
+      return J_B_bessel(y_squared, 1E-7, 1E-7, 10000, false);
+   } else if (n == 1) {
+      return D1_J_B_bessel(y_squared, 1E-7, 1E-7, 10000);
+   } else if (n == 2) {
+      return D2_J_B_bessel(y_squared, 1E-7, 1E-7, 10000);
+   } else {
+      return -1;
+   }
 }
 
 int main(int argc, char *argv[]) {
--- src/math.tm
+++ src/math.tm
@@ -1,15 +1,15 @@
 :Begin:
 :Function:      J_F
-:Pattern:       JF[ysq_?NumericQ]
-:Arguments:     {ysq}
-:ArgumentTypes: {Real}
+:Pattern:       JF[ysq_?NumericQ, derivative_Integer: 0]
+:Arguments:     {ysq, derivative}
+:ArgumentTypes: {Real, Integer}
 :ReturnType:    Real
 :End:
 
 :Begin:
 :Function:      J_B
-:Pattern:       JB[ysq_?NumericQ]
-:Arguments:     {ysq}
-:ArgumentTypes: {Real}
+:Pattern:       JB[ysq_?NumericQ, derivative_Integer: 0]
+:Arguments:     {ysq, derivative}
+:ArgumentTypes: {Real, Integer}
 :ReturnType:    Real
 :End:
