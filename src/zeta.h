a3c3cdb45f3e4e48cd6d82c95840c56cb9fd403c
Andrew Fowlie <andrew.j.fowlie@qq.com>
2019-10-30 17:11:45 +0800

    dont extern c wrap non c types (cdouble) as they wouldnt work anyway

--- src/zeta.h
+++ src/zeta.h
@@ -11,15 +11,7 @@
 /*! A complex double */
 typedef std::complex<double> cdouble;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 cdouble hurwitz_zeta(double s, cdouble a, int N = 50);
 cdouble polylog(double s, cdouble a, int N = 50);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif  // _ZETA_H_
